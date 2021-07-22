052928a08daafb7029a92779eeeeee6e0c560d36
EveSithole <evestholay@gmail.com>
2021-07-22 12:34:45 +0200

    edited file

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -99,7 +99,7 @@ def main():
                 # st.title("We think you'll like:")
                 # for i,j in enumerate(top_recommendations):
                 #     st.subheader(str(i+1)+'. '+j)  
-
+                
                 try:
                     with st.spinner('Crunching the numbers...'):
                         top_recommendations = collab_model(movie_list=fav_movies,
@@ -107,8 +107,7 @@ def main():
                     st.title("We think you'll like:")
                     for i,j in enumerate(top_recommendations):
                         st.subheader(str(i+1)+'. '+j)
-                 except:
-
+                except:
                     st.error("Oops! Looks like this algorithm does't work.\
                               We'll need to fix it!")
 
