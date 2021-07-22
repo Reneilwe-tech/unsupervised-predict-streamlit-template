5d344e1ea2542e4fb99bbedbc1c893723505b154
EveSithole <evestholay@gmail.com>
2021-07-22 12:20:08 +0200

    edited file

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -93,14 +93,22 @@ def main():
 
         if sys == 'Collaborative Based Filtering':
             if st.button("Recommend"):
-                try:
-                    with st.spinner('Crunching the numbers...'):
-                        top_recommendations = collab_model(movie_list=fav_movies,
+                with st.spinner('Crunching the numbers...'):
+                    top_recommendations = collab_model(movie_list=fav_movies,
                                                            top_n=10)
-                    st.title("We think you'll like:")
-                    for i,j in enumerate(top_recommendations):
-                        st.subheader(str(i+1)+'. '+j)
-                # except:
+                st.title("We think you'll like:")
+                for i,j in enumerate(top_recommendations):
+                    st.subheader(str(i+1)+'. '+j)  
+
+                # try:
+                #     with st.spinner('Crunching the numbers...'):
+                #         top_recommendations = collab_model(movie_list=fav_movies,
+                #                                            top_n=10)
+                #     st.title("We think you'll like:")
+                #     for i,j in enumerate(top_recommendations):
+                #         st.subheader(str(i+1)+'. '+j)
+                #  except:
+
                 #     st.error("Oops! Looks like this algorithm does't work.\
                 #               We'll need to fix it!")
 
